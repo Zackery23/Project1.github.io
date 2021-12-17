2dd38b03d8cca35f79cccbcd7a136e7481d5b7ef
Zackery McDaniel <81925313+Zackery23@users.noreply.github.com>
2021-12-16 23:58:52 -0500

    Update ProjectHTML1.js

--- ProjectHTML1.js
+++ ProjectHTML1.js
@@ -56,9 +56,9 @@ function initUpdatePrice(){
     }
 }
 class statInput{
-    constructor(level, initStrMod, initDexMod, initConMod, initIntMod, initWisMod
+    constructor(initProfecMod, initStrMod, initDexMod, initConMod, initIntMod, initWisMod
         , initChaMod) {
-            this.classLevel = level;
+            this.profecMod = initProfecMod;
             this.strMod = initStrMod;
             this.dexMod = initDexMod;
             this.conMod = initConMod;
@@ -69,19 +69,295 @@ class statInput{
 
     
     }
-    inputChar(){
-        this.level = document.getElementById("classLvl").value;
+    hideInfo(){
+        var article = document.getElementById('characterInfo');
+        article.style.display = "none";
+    }
+    rollSkill(clicked_id){
+        this.initProfecMod = document.getElementById("profecMod").value;
         this.initStrMod = document.getElementById("strMod").value;
         this.initDexMod = document.getElementById("dexMod").value;
         this.initConMod = document.getElementById("conMod").value;
         this.initIntMod = document.getElementById("intMod").value;
-        this.initWisMod = document.getElementById("wisMod").value;
+        this.initWisMod  = document.getElementById("wisMod").value;
         this.initChaMod = document.getElementById("chaMod").value;
-        this.testing(this.level);
+
+        if(clicked_id == 'acroButton'){
+            if(document.getElementById("Arcobatics").checked){
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initProfecMod) + parseInt(this.initDexMod);
+                let txt20Output = document.getElementById('arcoOut');
+                txt20Output.value = die20;
+            }
+            else{
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initDexMod);
+                let txt20Output = document.getElementById('arcoOut');
+                txt20Output.value = die20;
+            }
+            
+        }
+        if(clicked_id == 'AnimalButton'){
+            if(document.getElementById("animalHandling").checked){
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initProfecMod) + parseInt(this.initWisMod);
+                let txt20Output = document.getElementById('animalOut');
+                txt20Output.value = die20;
+            }
+            else{
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initWisMod);
+                let txt20Output = document.getElementById('animalOut');
+                txt20Output.value = die20;
+            }
+            
+        }
+        if(clicked_id == 'ArcanButton'){
+            if(document.getElementById("arcana").checked){
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initProfecMod) + parseInt(this.initIntMod);
+                let txt20Output = document.getElementById('animalOut');
+                txt20Output.value = die20;
+            }
+            else{
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initIntMod);
+                let txt20Output = document.getElementById('arcanaOut');
+                txt20Output.value = die20;
+            }
+            
+        }
+        if(clicked_id == 'AthletButton'){
+            if(document.getElementById("athletics").checked){
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initProfecMod) + parseInt(this.initStrMod);
+                let txt20Output = document.getElementById('athletOut');
+                txt20Output.value = die20;
+            }
+            else{
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initStrMod);
+                let txt20Output = document.getElementById('athletOut');
+                txt20Output.value = die20;
+            }
+            
+        }
+        if(clicked_id == 'decepButton'){
+            if(document.getElementById("deception").checked){
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initProfecMod) + parseInt(this.initChaMod);
+                let txt20Output = document.getElementById('decepOut');
+                txt20Output.value = die20;
+            }
+            else{
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initChaMod);
+                let txt20Output = document.getElementById('decepOut');
+                txt20Output.value = die20;
+            }
+            
+        }
+        if(clicked_id == 'histButton'){
+            if(document.getElementById("history").checked){
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initProfecMod) + parseInt(this.initIntMod);
+                let txt20Output = document.getElementById('historyOut');
+                txt20Output.value = die20;
+            }
+            else{
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initIntMod);
+                let txt20Output = document.getElementById('historyOut');
+                txt20Output.value = die20;
+            }
+            
+        }
+        if(clicked_id == 'insightButton'){
+            if(document.getElementById("insight").checked){
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initProfecMod) + parseInt(this.initWisMod);
+                let txt20Output = document.getElementById('insightOut');
+                txt20Output.value = die20;
+            }
+            else{
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initWisMod);
+                let txt20Output = document.getElementById('insightOut');
+                txt20Output.value = die20;
+            }
+            
+        }
+        if(clicked_id == 'intimButton'){
+            if(document.getElementById("intimidation").checked){
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initProfecMod) + parseInt(this.initChaMod);
+                let txt20Output = document.getElementById('intimidOut');
+                txt20Output.value = die20;
+            }
+            else{
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initChaMod);
+                let txt20Output = document.getElementById('intimidOut');
+                txt20Output.value = die20;
+            }
+            
+        }
+        if(clicked_id == 'investButton'){
+            if(document.getElementById("investigation").checked){
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initProfecMod) + parseInt(this.initIntMod);
+                let txt20Output = document.getElementById('investOut');
+                txt20Output.value = die20;
+            }
+            else{
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initIntMod);
+                let txt20Output = document.getElementById('investOut');
+                txt20Output.value = die20;
+            }
+            
+        }
+        if(clicked_id == 'medicButton'){
+            if(document.getElementById("medicine").checked){
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initProfecMod) + parseInt(this.initWisMod);
+                let txt20Output = document.getElementById('medicineOut');
+                txt20Output.value = die20;
+            }
+            else{
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initWisMod);
+                let txt20Output = document.getElementById('medicineOut');
+                txt20Output.value = die20;
+            }
+            
+        }
+        if(clicked_id == 'naturButton'){
+            if(document.getElementById("nature").checked){
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initProfecMod) + parseInt(this.initIntMod);
+                let txt20Output = document.getElementById('natureOut');
+                txt20Output.value = die20;
+            }
+            else{
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initIntMod);
+                let txt20Output = document.getElementById('natureOut');
+                txt20Output.value = die20;
+            }
+            
+        }
+        if(clicked_id == 'percepButton'){
+            if(document.getElementById("percep").checked){
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initProfecMod) + parseInt(this.initWisMod);
+                let txt20Output = document.getElementById('percepOut');
+                txt20Output.value = die20;
+            }
+            else{
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initWisMod);
+                let txt20Output = document.getElementById('percepOut');
+                txt20Output.value = die20;
+            }
+            
+        }
+        if(clicked_id == 'peformButton'){
+            if(document.getElementById("performance").checked){
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initProfecMod) + parseInt(this.initChaMod);
+                let txt20Output = document.getElementById('performOut');
+                txt20Output.value = die20;
+            }
+            else{
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initChaMod);
+                let txt20Output = document.getElementById('performOut');
+                txt20Output.value = die20;
+            }
+            
+        }
+        if(clicked_id == 'persuaButton'){
+            if(document.getElementById("persuade").checked){
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initProfecMod) + parseInt(this.initChaMod);
+                let txt20Output = document.getElementById('persuadeOut');
+                txt20Output.value = die20;
+            }
+            else{
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initChaMod);
+                let txt20Output = document.getElementById('persuadeOut');
+                txt20Output.value = die20;
+            }
+            
+        }
+        if(clicked_id == 'religionButton'){
+            if(document.getElementById("religion").checked){
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initProfecMod) + parseInt(this.initIntMod);
+                let txt20Output = document.getElementById('religionOut');
+                txt20Output.value = die20;
+            }
+            else{
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initIntMod);
+                let txt20Output = document.getElementById('religionOut');
+                txt20Output.value = die20;
+            }
+            
+        }
+        if(clicked_id == 'sleightButton'){
+            if(document.getElementById("sleight").checked){
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initProfecMod) + parseInt(this.initDexMod);
+                let txt20Output = document.getElementById('sleightOut');
+                txt20Output.value = die20;
+            }
+            else{
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initDexMod);
+                let txt20Output = document.getElementById('sleightOut');
+                txt20Output.value = die20;
+            }
+            
+        }
+        if(clicked_id == 'stealthButton'){
+            if(document.getElementById("sleight").checked){
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initProfecMod) + parseInt(this.initDexMod);
+                let txt20Output = document.getElementById('stealthOut');
+                txt20Output.value = die20;
+            }
+            else{
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initDexMod);
+                let txt20Output = document.getElementById('stealthOut');
+                txt20Output.value = die20;
+            }
+            
+        }
+        if(clicked_id == 'survivalButton'){
+            if(document.getElementById("survival").checked){
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initProfecMod) + parseInt(this.initWisMod);
+                let txt20Output = document.getElementById('survialOut');
+                txt20Output.value = die20;
+            }
+            else{
+                let die20 = Math.floor(Math.random() * 20) + 1;
+                die20 = die20 + parseInt(this.initWisMod);
+                let txt20Output = document.getElementById('survialOut');
+                txt20Output.value = die20;
+            }
+            
+        }
     }
-    skill(level){
-        level.value;
-        console.log(level);
+    hideSkills(){
+        let hide = document.getElementsByClassName("hide_skills");
+        for (let el of hide) {
+            el.value = "";
+        }
     }
 }
 let In = new statInput(0, 0, 0, 0, 0, 0)
